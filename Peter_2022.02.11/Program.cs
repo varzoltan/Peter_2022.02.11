8aaa4da4f0b8e17115bc4422638ce3a921a53201
varzoltan <64203391+varzoltan@users.noreply.github.com>
2022-02-17 16:53:58 +0100

    16:53

--- Peter_2022.02.11/Program.cs
+++ Peter_2022.02.11/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Peter_2022._02._11
 {
     internal class Program
     {
+        static string hetnapja(int honap, int nap)
+        {
+            string[] napnev = { "vasarnap", "hetfo", "kedd", "szerda", "csutortok", "pentek", "szombat" };
+            int[] napszam = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 335 };
+            int napsorszam = (napszam[honap - 1] + nap) % 7;
+            string hetnapja = napnev[napsorszam];
+            return hetnapja;
+        }
+
+        static int osszead(int a,int b)
+        {
+            a = 10;
+            b = 20;
+            int osszesen = a + b;
+            int szorzata = a * b;
+            return szorzata;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(osszead(40,50));
+            int honap = 4;
+            int nap = 4;
+            Console.WriteLine(hetnapja(honap,nap));
             //Tömbök
             //Egydimenziós tömbök: vektor
             //1-Ismerem az elemeit
@@ -49,6 +72,16 @@ namespace Peter_2022._02._11
             }
             Console.WriteLine(tomb3[2,4]);
 
+            //szorzótábla
+            //int[,] tomb4 = new int[10,10];
+            for (int i = 1;i<11;i++)
+            {
+                for (int j = 1;j<11;j++)
+                {
+                    Console.Write((i*j).ToString("##0"));
+                }
+                Console.WriteLine();
+            }
             Console.ReadKey();
         }
     }
